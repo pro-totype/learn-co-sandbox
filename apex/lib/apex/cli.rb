dd2ba99ade2228af33dd23ffe850bc4ff81674b0
Sean B <butts.sean01@gmail.com>
2020-08-28 01:03:13 +0000

    Automatically backed up by Learn

--- apex/lib/apex/cli.rb
+++ apex/lib/apex/cli.rb
@@ -1,19 +1,17 @@
-class CLI 
-  
-  def initialize(path = "./db/mp3s")
-    MusicImporter.new(path).import
-  end
+class CLI
 
-  def charcter_class
-    puts "Choose Character Role: Defense, Offense, Recon, Support"
-    input = gets.chomp 
-    puts
-
-
-
-  
   def start 
     puts "Welcome to the Apex Character Database"
     puts "Please select a character role"
   end
 end
+
+  def charcter_class
+    puts "Choose Character Role: Defense, Offense, Recon, Support"
+    role = gets 
+    role = gets.chomp 
+  end
+
+
+
+  
